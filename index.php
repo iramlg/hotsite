b6af8faa5ea3ebce3a2f512d7a1610d547ef19e1
Iram Garcia <iramlg@hotmail.com>
2012-02-24 10:01:07 -0200

    criacao de repositorio

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Golden Tulip</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<style type="text/css">
+  
+</style>
+</head>
+<body>
+    <div id="center">
+        <div id="header">
+        </div>
+        <div id="categoria">
+            <h2>Noite de núpcias</h2>
+            <h2>Lua de Mel</h2>
+            <h2>Espaço para Eventos</h2>
+        </div>
+        <div id="bg1">
+        </div>
+        <div id="estado">
+        	<p>Escolha um estado</p>
+        	<form name="Menu">
+			 	<select name="pages" size="1" onChange="vai(this);">
+			  		<option>1 - inclusao de Titular</option> 
+					<option>2 - Inclusao de Dependente</option> 
+					<option>3 - Exclusao de Titular</option> 
+					<option>4 - Exclusao de Dependente</option> 
+					<option>5 - Solicitacao de 2 via</option> 
+					<option>8 - Alteracao de Dados</option> 
+			 	</select>
+			</form>
+        </div>
+        <div id="bg2">
+        </div>
+        <div id="hoteis">
+        	<ul>
+        		<li></li>
+        	</ul>
+        </div>
+        <div id="bg3">
+        </div>
+        <div id="data">
+        	<label>Escolha a data</laber>
+        	<input></input>
+        	<label>Noites</laber>
+        	<input></input>
+        </div>
+        <div id="footer">
+        	<div>
+        		<label>Nome</label>
+        		<input></input>
+        	</div>
+        </div>
+    </div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<script>
+
+</script>
+
+</html>
